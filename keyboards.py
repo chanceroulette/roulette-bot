bfeeace6d578babb1597fdd391e660599ce8f52e
chanceroulette <cudiaf@gmail.com>
2025-04-19 15:27:33 +0400

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,20 +1,17 @@
-# keyboards.py
-
 from telegram import ReplyKeyboardMarkup
 
-def menu_keyboard():
-    return ReplyKeyboardMarkup([
-        ["/help", "/menu", "/id"],
-        ["/storico", "/report", "/annulla_ultima"],
-        ["/primi15", "/modalita_inserimento", "/admin"],
-    ], resize_keyboard=True)
+def main_menu_keyboard(is_admin: bool = False):
+    buttons = [
+        ["/set_bankroll", "/primi15", "/modalita_inserimento"],
+        ["/report", "/storic o", "/annulla_ultima"],
+        ["/help", "/id", "/end_session"]
+    ]
+    if is_admin:
+        buttons.append(["/admin"])
+    return ReplyKeyboardMarkup(buttons, resize_keyboard=True)
 
 def number_keyboard():
-    numeri = list(map(str, range(0, 37)))
-    tastiera = []
-
-    for i in range(0, 36, 6):
-        tastiera.append(numeri[i:i+6])
-
-    tastiera.append(["0", "Annulla", "Menu"])
-    return ReplyKeyboardMarkup(tastiera, resize_keyboard=True)
+    nums = [str(i) for i in range(0,37)]
+    kb = [nums[i:i+6] for i in range(0, 36, 6)]
+    kb.append(["Annulla", "Menu"])
+    return ReplyKeyboardMarkup(kb, resize_keyboard=True)
