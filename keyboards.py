9058d5d45f8df72d92618b34fb69bddd6b439a63
chanceroulette <cudiaf@gmail.com>
2025-04-19 15:01:09 +0400

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,27 +1,20 @@
+# keyboards.py
+
 from telegram import ReplyKeyboardMarkup
 
-def main_menu_keyboard():
-    return ReplyKeyboardMarkup(
-        [
-            ["/primi15", "/report", "/storico"],
-            ["/annulla_ultima", "/menu", "/help"]
-        ],
-        resize_keyboard=True
-    )
+def menu_keyboard():
+    return ReplyKeyboardMarkup([
+        ["/help", "/menu", "/id"],
+        ["/storico", "/report", "/annulla_ultima"],
+        ["/primi15", "/modalita_inserimento", "/admin"],
+    ], resize_keyboard=True)
 
-def roulette_keyboard():
+def number_keyboard():
+    numeri = list(map(str, range(0, 37)))
     tastiera = []
-    numeri = list(range(1, 37))
-    for i in range(0, 36, 6):
-        tastiera.append([str(n) for n in numeri[i:i+6]])
-    tastiera.append(["0"])
-    return ReplyKeyboardMarkup(tastiera, resize_keyboard=True)
 
-def build_chances_keyboard(chances=None):
-    tutte = ["rosso", "nero", "pari", "dispari", "manque", "passe"]
-    if chances:
-        righe = [[c.capitalize()] for c in chances]
-    else:
-        righe = [[c.capitalize()] for c in tutte]
-    righe.append(["Conferma"])
-    return ReplyKeyboardMarkup(righe, resize_keyboard=True)
+    for i in range(0, 36, 6):
+        tastiera.append(numeri[i:i+6])
+
+    tastiera.append(["0", "Annulla", "Menu"])
+    return ReplyKeyboardMarkup(tastiera, resize_keyboard=True)
